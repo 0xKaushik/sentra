4badd50e8d76671796f6532e8f407ca7c04eba55
kaushik <kaushik.exe@protonmail.com>
2025-06-06 15:08:16 +0530

    6/6/25

---
+++ sentra/try_temp.py
@@ -0,0 +1,14 @@
+import anthropic
+
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key="***REMOVED***",
+)
+message = client.messages.create(
+    model="claude-3-7-sonnet-20250219",
+    max_tokens=1024,
+    messages=[
+        {"role": "user", "content": "tell me about the vunerability in windows 11"}
+    ]
+)
+print(message.content)
